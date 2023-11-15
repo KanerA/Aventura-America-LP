48c55557fd9746d38424b80f7c591f42420677c8
kanera <kaner.assaf@gmail.com>
2023-11-15 05:22:07 +0200

    first layout of landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-// import reactLogo from './assets/react.svg';
+import "./App.css";
+import logo from './assets/icon.svg';
+
 
 const App = () => {
 
-  return <div>
-    Aventura America
-  </div>
+  return <>
+    <div className="background"></div>
+    <div className="pageContainer">
+      <div className="header">
+        <div className="logoContainer">
+          <img src={logo} alt="aventura america logo" />
+        </div>
+      </div>
+      <div className="mainSection">
+        <h1>WE ARE LAUNCHING SOON!</h1>
+        <h5>Subscribe and get notified</h5>
+        <div className="subscriptionInputs">
+          <div className="inputContainer">
+            <input type="text" name="email" id="emailInput" />
+            <button type="submit" className="slide_right">Notify Me!</button>
+          </div>
+        </div>
+      </div>
+      <div className="footer">
+        social media links...
+      </div>
+    </div>
+  </>
 };
 
 export default App;
